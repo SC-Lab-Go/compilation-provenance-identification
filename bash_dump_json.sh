a088b744d67b0d20875d083607dc601dfb226b9f
Irfan <mi0214@lxg7pllm3-cse.unt.ad.unt.edu>
2022-05-11 18:31:34 -0500

    test push

---
+++ bash_dump_json.sh
@@ -0,0 +1,10 @@
+
+
+#python build_binary_acfg.py /home/yuedeji/data/cyber_data/malware_sei/malware_processed /home/yuedeji/data/cyber_data/malware_sei/meta_data/binary_label.csv /home/yuedeji/data/cyber_data/malware_sei/malware_json
+
+home=/home/UNT/mi0214/vestige/binall_snns_post
+output_folder=/home/UNT/mi0214/feature_nestgnn/json_folder
+
+
+
+python modify_build_binary_acfg.py $home $output_folder
